d91945bd2807c1d711f5816c7b5ce8517b1d2750
Marcus Ventin <94906498+marcusventin@users.noreply.github.com>
2022-02-18 20:25:59 +0000

    Added and tested top-up functionality

--- lib/oystercard.py
+++ lib/oystercard.py
@@ -1,3 +1,9 @@
 class Oystercard():
     def __init__(self):
         self.balance = 0
+    
+    def top_up(self, top_up):
+        if type(top_up) != int:
+            raise Exception("Top up value must be a whole number.")
+        else:
+            self.balance += top_up
--- tests/test_oystercard.py
+++ tests/test_oystercard.py
@@ -1,6 +1,15 @@
+from random import randint
+
 from lib.oystercard import Oystercard
 
 class TestOystercard():
     def test_has_balance_attribute(self):
         card = Oystercard()
         assert card.balance == 0
+    
+    def test_top_up_adds_to_balance(self):
+        card = Oystercard()
+        assert card.balance == 0
+        number = randint(1, 100)
+        card.top_up(number)
+        assert card.balance == number
