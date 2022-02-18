a7df120eb4c49d211b86f5fc90ddf372db724efd
Marcus Ventin <94906498+marcusventin@users.noreply.github.com>
2022-02-18 20:32:04 +0000

    Added maximum balance

--- lib/oystercard.py
+++ lib/oystercard.py
@@ -1,9 +1,15 @@
 class Oystercard():
+    MAXIMUM_BALANCE = 90
+
     def __init__(self):
         self.balance = 0
     
     def top_up(self, top_up):
         if type(top_up) != int:
             raise Exception("Top up value must be a whole number.")
+        if top_up <= 0:
+            raise Exception("Top up value must be positive.")
+        if self.balance + top_up > self.MAXIMUM_BALANCE:
+            raise Exception("You can't exceed the maximum balance.")
         else:
             self.balance += top_up
--- tests/test_oystercard.py
+++ tests/test_oystercard.py
@@ -1,4 +1,5 @@
 from random import randint
+import pytest
 
 from lib.oystercard import Oystercard
 
@@ -10,6 +11,14 @@ class TestOystercard():
     def test_top_up_adds_to_balance(self):
         card = Oystercard()
         assert card.balance == 0
-        number = randint(1, 100)
+        number = randint(1, card.MAXIMUM_BALANCE)
         card.top_up(number)
         assert card.balance == number
+    
+    def test_top_up_raises_error_if_exceeds_maximum_balance(self):
+        card = Oystercard()
+        card.top_up(card.MAXIMUM_BALANCE)
+        with pytest.raises(Exception,
+            match = "You can't exceed the maximum balance."):
+            card.top_up(1)
+
